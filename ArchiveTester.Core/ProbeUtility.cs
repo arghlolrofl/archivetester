5a98af3ff3db37e9a326b02d6b5f219617480ac3
neomon <maryjoana@eclipso.me>
2015-12-24 13:16:22 +0100

    Merging

--- ArchiveTester.Core/ProbeUtility.cs
+++ ArchiveTester.Core/ProbeUtility.cs
@@ -1,54 +1,47 @@
 ﻿using System;
 using ArchiveTester.Core.Contracts;
 
-namespace ArchiveTester.Core
-{
-  public class ProbeUtility : IProbeUtility
-  {
-    Func<string> _nextStringAction;
+namespace ArchiveTester.Core {
+    public class ProbeUtility : IProbeUtility {
+        Func<string> _nextStringAction;
 
-    public IArchiveHandler ArchiveHandler { get; set; }
+        public IArchiveHandler ArchiveHandler { get; set; }
 
-    /// <summary>
-    /// Constructor
-    /// </summary>
-    /// <param name="handler"></param>
-    public ProbeUtility(IArchiveHandler handler, Func<string> action = null)
-    {
-      ArchiveHandler = handler;
-      _nextStringAction = action;
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="handler"></param>
+        public ProbeUtility(IArchiveHandler handler, Func<string> action = null) {
+            ArchiveHandler = handler;
+            _nextStringAction = action;
+        }
+
+        public bool Run() {
+            string nextPass = null;
+            do {
+                nextPass = _nextStringAction.Invoke();
+                if (String.IsNullOrEmpty(nextPass))
+                    return false;
+
+                RaiseTestingPassword(nextPass);
+            } while (!ArchiveHandler.TestPassword(nextPass));
+
+            return true;
+        }
+
+        #region EventHandlers
+
+        public event EventHandler<string> TestingPassword;
+        private void RaiseTestingPassword(string pw) {
+            EventHandler<string> handler = TestingPassword;
+            if (handler != null)
+                handler(this, pw);
+        }
+
+        #endregion
+
+        public void Dispose() {
+
+        }
     }
-
-    public bool Run()
-    {
-      string nextPass = null;
-      do
-      {
-        nextPass = _nextStringAction.Invoke();
-        if (String.IsNullOrEmpty(nextPass))
-          return false;
-
-        RaiseTestingPassword(nextPass);
-      } while (!ArchiveHandler.TestPassword(nextPass));
-
-      return true;
-    }
-
-    #region EventHandlers
-
-    public event EventHandler<string> TestingPassword;
-    private void RaiseTestingPassword(string pw)
-    {
-      EventHandler<string> handler = TestingPassword;
-      if (handler != null)
-        handler(this, pw);
-    }
-
-    #endregion
-
-    public void Dispose()
-    {
-
-    }
-  }
 }
--- ArchiveTester/Program.cs
+++ ArchiveTester/Program.cs
@@ -5,113 +5,98 @@ using System.Threading.Tasks;
 using ArchiveTester.Core;
 using ArchiveTester.Core.Contracts;
 
-namespace ArchiveTester
-{
-  class Program
-  {
-    const string CONFIG_PATH = "..\\..\\..\\config.cfg";
+namespace ArchiveTester {
+    class Program {
+        const string CONFIG_PATH = "..\\..\\..\\config.cfg";
 
-    static void Main(string[] args)
-    {
-      Console.WindowHeight = 48;
-      Console.WindowWidth = 128;
+        static void Main(string[] args) {
+            Console.WindowHeight = 48;
+            Console.WindowWidth = 128;
 
-      try
-      {
-        Program p = new Program().Initialize();
-        p.Run();
-      }
-      catch (Exception ex)
-      {
-        Console.WriteLine(ex.GetType().Name + ": " + ex.Message);
-      }
+            try {
+                Program p = new Program().Initialize();
+                p.Run();
+            } catch (Exception ex) {
+                Console.WriteLine(ex.GetType().Name + ": " + ex.Message);
+            }
+        }
+
+        public IList<IProbeUtility> Probes { get; set; }
+        public ITestString TestString { get; set; }
+
+
+        private Program Initialize() {
+            Console.Clear();
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine("############################################################################");
+            Console.WriteLine("# Password Test");
+            Console.WriteLine("############################################################################");
+            Console.WriteLine("#");
+            Console.WriteLine("#     Password: ");
+            Console.WriteLine("#");
+            Console.WriteLine("############################################################################");
+            Console.CancelKeyPress += Console_CancelKeyPress;
+
+            prepareTestString();
+            setupProbes();
+
+            return this;
+        }
+
+        public void Run() {
+            Task<bool>[] tasks = new Task<bool>[Probes.Count];
+            for (int i = 0; i < Probes.Count; i++)
+                tasks[i] = Task.Factory.StartNew(() => Probes[i].Run());
+
+            Task.WaitAll(tasks);
+        }
+
+        private void prepareTestString() {
+            FileInfo configFile = new FileInfo(CONFIG_PATH);
+
+            if (!configFile.Exists) {
+                configFile.Create();
+                TestString = new TestString(6);
+            } else if (configFile.Length > 0) {
+                using (StreamReader sr = configFile.OpenText())
+                    TestString = new TestString(6, sr.ReadToEnd());
+            }
+        }
+
+        private void setupProbes() {
+            Probes = new List<IProbeUtility>();
+
+            string[] commandLineArgs = Environment.GetCommandLineArgs();
+
+            if (commandLineArgs.Length <= 1 || string.IsNullOrEmpty(commandLineArgs[1]))
+                throw new ArgumentNullException("No path parameter supplied!");
+
+            IList<string> archivePathes = new List<string>(commandLineArgs.Length - 1);
+            for (int i = 0; i < commandLineArgs.Length - 1; i++)
+                archivePathes.Add(commandLineArgs[i + 1]);
+
+            foreach (string path in archivePathes) {
+                FileInfo targetFile = new FileInfo(path);
+                if (!targetFile.Exists)
+                    throw new FileNotFoundException("File not found: " + targetFile.FullName);
+
+                IArchiveHandler archiveHandler = new ArchiveHandler(targetFile);
+                IProbeUtility probe = new ProbeUtility(archiveHandler, TestString.GetNext);
+                probe.TestingPassword += Probe_TestingPassword;
+
+                Probes.Add(probe);
+            }
+        }
+
+        private void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e) {
+            foreach (var probe in Probes)
+                if (probe != null) 
+                    probe.Dispose();
+        }
+
+        private void Probe_TestingPassword(object sender, string e) {
+            Console.SetCursorPosition(16, 4);
+            Console.Write(e);
+        }
     }
-
-    public IList<IProbeUtility> Probes { get; set; }
-    public ITestString TestString { get; set; }
-
-
-    private Program Initialize()
-    {
-      Console.Clear();
-      Console.SetCursorPosition(0, 0);
-      Console.WriteLine("############################################################################");
-      Console.WriteLine("# Password Test");
-      Console.WriteLine("############################################################################");
-      Console.WriteLine("#");
-      Console.WriteLine("#     Password: ");
-      Console.WriteLine("#");
-      Console.WriteLine("############################################################################");
-      Console.CancelKeyPress += Console_CancelKeyPress;
-
-      prepareTestString();
-      setupProbes();
-
-      return this;
-    }
-
-    public void Run()
-    {
-      Task<bool>[] tasks = new Task<bool>[Probes.Count];
-      for (int i = 0; i < Probes.Count; i++)
-        tasks[i] = Task.Factory.StartNew(() => Probes[i].Run());
-
-      Task.WaitAll(tasks);
-    }
-
-    private void prepareTestString()
-    {
-      FileInfo configFile = new FileInfo(CONFIG_PATH);
-
-      if (!configFile.Exists)
-      {
-        configFile.Create();
-        TestString = new TestString(6);
-      }
-      else if (configFile.Length > 0)
-      {
-        using (StreamReader sr = configFile.OpenText())
-          TestString = new TestString(6, sr.ReadToEnd());
-      }
-    }
-
-    private void setupProbes()
-    {
-      Probes = new List<IProbeUtility>();
-
-      string[] commandLineArgs = Environment.GetCommandLineArgs();
-
-      if (commandLineArgs.Length <= 1 || string.IsNullOrEmpty(commandLineArgs[1]))
-        throw new ArgumentNullException("No path parameter supplied!");
-
-      IList<string> archivePathes = new List<string>(commandLineArgs.Length - 1);
-      for (int i = 0; i < commandLineArgs.Length - 1; i++)
-        archivePathes.Add(commandLineArgs[i + 1]);
-
-      foreach (string path in archivePathes)
-      {
-        FileInfo targetFile = new FileInfo(path);
-        if (!targetFile.Exists)
-          throw new FileNotFoundException("File not found: " + targetFile.FullName);
-
-        IArchiveHandler archiveHandler = new ArchiveHandler(targetFile);
-        IProbeUtility probe = new ProbeUtility(archiveHandler, TestString.GetNext);
-        probe.TestingPassword += Probe_TestingPassword;
-
-        Probes.Add(probe);
-      }
-    }
-
-    private void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
-    {
-      foreach (var probe in Probes)
-        probe?.Dispose();
-    }
-
-    private void Probe_TestingPassword(object sender, string e)
-    {
-      Console.SetCursorPosition(16, 4);
-      Console.Write(e);
-    }
-  }
 }
